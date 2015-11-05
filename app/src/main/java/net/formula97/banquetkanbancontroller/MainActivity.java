c3f8b7db71e37aabd69af76c3bc7fe41f9416b87
HAJIME Fukuna <f97one@gmail.com>
2015-11-05 23:24:32 +0900

    とりあえず宴会サマリを取得してListViewに張るまでを追加。

--- app/src/main/java/net/formula97/banquetkanbancontroller/MainActivity.java
+++ app/src/main/java/net/formula97/banquetkanbancontroller/MainActivity.java
@@ -1,13 +1,39 @@
 package net.formula97.banquetkanbancontroller;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import net.formula97.banquetkanbancontroller.adapters.SummaryListAdapter;
+import net.formula97.banquetkanbancontroller.dto.BanquetSummary;
+import net.formula97.banquetkanbancontroller.models.BanquetSummaryModel;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.summaryList)
+    ListView summaryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        BanquetSummaryModel model = new BanquetSummaryModel(getApplicationContext());
+        try {
+            List<BanquetSummary> banquetSummaries = model.findAll();
+
+            SummaryListAdapter adapter = new SummaryListAdapter(this, R.layout.sumamry_row, banquetSummaries);
+            summaryList.setAdapter(adapter);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
